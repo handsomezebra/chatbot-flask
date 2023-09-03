6209017c515afad700d280edf32c48c45078dc74
Huajun Zeng <hjzeng@gmail.com>
2023-09-03 11:56:36 -0700

    Make it work with OpenAI

--- src/flaskapp/chat.py
+++ src/flaskapp/chat.py
@@ -7,22 +7,6 @@ from flask import Blueprint, Response, current_app, render_template, request, st
 
 bp = Blueprint("chat", __name__, template_folder="templates", static_folder="static")
 
-# Configure OpenAI API
-openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT")
-openai.api_version = "2023-03-15-preview"
-if os.getenv("AZURE_OPENAI_KEY"):
-    openai.api_type = "azure"
-    openai.api_key = os.getenv("AZURE_OPENAI_KEY")
-else:
-    openai.api_type = "azure_ad"
-    if os.getenv("AZURE_OPENAI_CLIENT_ID"):
-        default_credential = azure.identity.ManagedIdentityCredential(client_id=os.getenv("AZURE_OPENAI_CLIENT_ID"))
-    else:
-        default_credential = azure.identity.DefaultAzureCredential(exclude_shared_token_cache_credential=True)
-    token = default_credential.get_token("https://cognitiveservices.azure.com/.default")
-    openai.api_key = token.token
-
-
 @bp.get("/")
 def index():
     return render_template("index.html")
@@ -35,7 +19,7 @@ def chat_handler():
     @stream_with_context
     def response_stream():
         response = openai.ChatCompletion.create(
-            engine=os.getenv("AZURE_OPENAI_CHATGPT_DEPLOYMENT", "chatgpt"),
+            model="gpt-3.5-turbo",
             messages=[
                 {"role": "system", "content": "You are a helpful assistant."},
                 {"role": "user", "content": request_message},
